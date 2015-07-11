9abe15eab3679cf2f1317aab79464d58ea5c5916
Wintus <132e84e5@opayq.com>
2015-07-11 10:35:32 -0700

    Create Triangle class

---
+++ src/Chapter1/Triangle.java
@@ -0,0 +1,20 @@
+package Chapter1;
+
+/**
+ * Problem of chapter 1
+ * Created by Wintus on 2015/07/10.
+ */
+public class Triangle {
+    int n;
+    int[] a;
+
+    public Triangle(int n, int[] a) {
+        this.n = n;
+        this.a = new int[n];
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
