2ac1bc69f6441f18cb3c83115cb1335d669e124b
Wintus <132e84e5@opayq.com>
2015-07-11 12:21:06 -0700

    Implement solve method

--- src/Chapter1/Lottery.java
+++ src/Chapter1/Lottery.java
@@ -1,5 +1,7 @@
 package Chapter1;
 
+import java.util.Arrays;
+
 /**
  * Problem of Chapter 1.
  * Created by Wintus on 2015/07/11.
@@ -11,5 +13,25 @@ public class Lottery {
     public Lottery(int sum, int[] lots) {
         this.sum = sum;
         this.lots = lots;
+    }
+
+    void solve() {
+        int n = lots.length;
+        int[] lots2 = new int[n * (n + 1) / 2];
+        for (int c = 0; c < n; c++) {
+            for (int d = c; d < n; d++) {
+                lots2[c * n + n - d] = lots[c] + lots[d];
+            }
+        }
+        Arrays.sort(lots2);
+        for (int a = 0; a < n; a++) {
+            for (int b = a; b < n; b++) {
+                if (Arrays.binarySearch(lots2, sum - lots[a] - lots[b]) >= 0) {
+                    System.out.println("Yes");
+                    return;
+                }
+            }
+        }
+        System.out.println("No");
     }
 }
