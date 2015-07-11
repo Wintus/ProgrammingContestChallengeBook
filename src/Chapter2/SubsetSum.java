ec5df60e58d61d2e94ebcff662a1d6a2dabee5dc
Wintus <132e84e5@opayq.com>
2015-07-11 13:50:55 -0700

    Implement solve method

--- src/Chapter2/SubsetSum.java
+++ src/Chapter2/SubsetSum.java
@@ -5,11 +5,22 @@ package Chapter2;
  * Created by Yuya on 2015/07/11.
  */
 public class SubsetSum {
-    int[] a;
-    int sum;
+    final int[] a;
+    final int sum;
 
     public SubsetSum(int[] a, int sum) {
         this.a = a;
         this.sum = sum;
+    }
+
+    boolean solve() {
+        return dfs(0, 0);
+    }
+
+    private boolean dfs(int n, int sum) {
+        if (n == a.length) return sum == this.sum;
+        if (dfs(n + 1, sum)) return true;
+        else if (dfs(n + 1, sum + a[n])) return true;
+        return false;
     }
 }
