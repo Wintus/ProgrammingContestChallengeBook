11d5bb692a1626e04862d8adaac488bf1565e209
Wintus <132e84e5@opayq.com>
2015-07-11 11:39:14 -0700

    Implement solve method

--- src/Chapter1/Ants.java
+++ src/Chapter1/Ants.java
@@ -1,5 +1,8 @@
 package Chapter1;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Problem of Chapter 1.
  * Created by Wintus on 2015/07/11.
@@ -11,5 +14,13 @@ public class Ants {
     public Ants(int l, int[] ants) {
         L = l;
         this.ants = ants;
+    }
+
+    void solve() {
+        int min;
+        int max;
+        min = Arrays.stream(ants).map(x -> Math.min(x, L - x)).max().getAsInt();
+        max = Arrays.stream(ants).map(x -> Math.max(x, L - x)).max().getAsInt();
+        System.out.printf("%d %d\n", min, max);
     }
 }
