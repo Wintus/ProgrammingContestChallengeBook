391f917efe3774afaba1760cec3c32f3420306fa
Wintus <132e84e5@opayq.com>
2015-07-11 11:05:32 -0700

    Implement main method

--- src/Chapter1/Triangle.java
+++ src/Chapter1/Triangle.java
@@ -1,5 +1,8 @@
 package Chapter1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 /**
@@ -28,6 +31,19 @@ public class Triangle {
     }
 
     public static void main(String[] args) {
-
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in))
+        ) {
+            String line = reader.readLine();
+            int n = Integer.parseInt(line);
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(reader.readLine());
+            }
+            Triangle triangle = new Triangle(n, a);
+            triangle.solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
