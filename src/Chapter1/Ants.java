0fde17b6a00ff26b090cd942065e9d9f341ed304
Wintus <132e84e5@opayq.com>
2015-07-11 11:30:14 -0700

    Create Ants class

---
+++ src/Chapter1/Ants.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+/**
+ * Problem of Chapter 1.
+ * Created by Wintus on 2015/07/11.
+ */
+public class Ants {
+    private int L;
+    private int[] ants;
+
+    public Ants(int l, int[] ants) {
+        L = l;
+        this.ants = ants;
+    }
+}
