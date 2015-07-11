6c1216f6c9bc0b8d1a9e79570b44be56e11c71d0
Wintus <132e84e5@opayq.com>
2015-07-11 13:36:25 -0700

    Create SubsetSum class

---
+++ src/Chapter2/SubsetSum.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+/**
+ * Chapter 2.
+ * Created by Yuya on 2015/07/11.
+ */
+public class SubsetSum {
+    int[] a;
+    int sum;
+
+    public SubsetSum(int[] a, int sum) {
+        this.a = a;
+        this.sum = sum;
+    }
+}
