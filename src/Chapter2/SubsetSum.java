82842f24d585578eb321d19634c069e31fb9e9c1
Wintus <132e84e5@opayq.com>
2015-07-11 13:56:19 -0700

    Implement main method

--- src/Chapter2/SubsetSum.java
+++ src/Chapter2/SubsetSum.java
@@ -1,5 +1,7 @@
 package Chapter2;
 
+import java.util.Scanner;
+
 /**
  * Chapter 2.
  * Created by Yuya on 2015/07/11.
@@ -22,5 +24,15 @@ class SubsetSum {
         if (dfs(n + 1, sum)) return true;
         else if (dfs(n + 1, sum + a[n])) return true;
         return false;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = scanner.nextInt();
+            int sum = scanner.nextInt();
+            System.out.println(new SubsetSum(a, sum).solve() ? "Yes" : "No");
+        }
     }
 }
