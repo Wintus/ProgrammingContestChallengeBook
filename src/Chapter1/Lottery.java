09bdec62e22d287343ba4514067aa66cf8d0f9f0
Wintus <132e84e5@opayq.com>
2015-07-11 12:07:31 -0700

    Create Lottery class

---
+++ src/Chapter1/Lottery.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+/**
+ * Problem of Chapter 1.
+ * Created by Wintus on 2015/07/11.
+ */
+public class Lottery {
+    int sum;
+    int[] lots;
+
+    public Lottery(int sum, int[] lots) {
+        this.sum = sum;
+        this.lots = lots;
+    }
+}
