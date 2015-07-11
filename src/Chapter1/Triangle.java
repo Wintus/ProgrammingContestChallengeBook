260624202fd5c84ecf9db097b2252e87602eede5
Wintus <132e84e5@opayq.com>
2015-07-11 10:53:20 -0700

    Implement solve method

--- src/Chapter1/Triangle.java
+++ src/Chapter1/Triangle.java
@@ -1,5 +1,7 @@
 package Chapter1;
 
+import java.util.Arrays;
+
 /**
  * Problem of chapter 1
  * Created by Wintus on 2015/07/10.
@@ -12,6 +14,17 @@ public class Triangle {
         this.n = n;
         this.a = new int[n];
         this.a = a;
+    }
+
+    void solve() {
+        Arrays.sort(a);
+        int max = 0;
+        for (int i = 2; i < n; i++) {
+            if (a[i] < a[i - 1] + a[i - 2]) {
+                max = Math.max(max, a[i] + a[i - 1] + a[i - 2]);
+            }
+        }
+        System.out.println(max);
     }
 
     public static void main(String[] args) {
