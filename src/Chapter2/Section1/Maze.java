1ff801a2f12addb13baec6dc2a70df3f131701e7
Wintus <132e84e5@opayq.com>
2015-07-11 18:15:52 -0700

    Create Maze class

---
+++ src/Chapter2/Section1/Maze.java
@@ -0,0 +1,13 @@
+package Chapter2.Section1;
+
+/**
+ * Using breadth first search.
+ * Created by Yuya on 2015/07/11.
+ */
+public class Maze {
+    char[][] maze;
+
+    public Maze(char[][] maze) {
+        this.maze = maze;
+    }
+}
