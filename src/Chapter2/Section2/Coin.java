f7802d1c10355420e456282b23b3a8a9d7f18c3e
Wintus <132e84e5@opayq.com>
2015-07-12 10:39:57 -0700

    Implement solve method

--- src/Chapter2/Section2/Coin.java
+++ src/Chapter2/Section2/Coin.java
@@ -1,5 +1,7 @@
 package Chapter2.Section2;
 
+import java.util.stream.IntStream;
+
 /**
  * æÃ—~–@ - greedy algorithm
  * Created by Yuya on 2015/07/12.
@@ -12,5 +14,19 @@ public class Coin {
     public Coin(int[] holding, int amount) {
         this.holding = holding;
         this.amount = amount;
+    }
+
+    /**
+     * Find the smallest number of coins to pay.
+     *
+     * @return the minimum number of coins.
+     */
+    int solve() {
+        return IntStream.range(0, coins.length)
+                .map(i -> {
+                    int n = Math.min(amount / coins[i], holding[i]);
+                    amount -= coins[i] * n;
+                    return n;
+                }).sum();
     }
 }
