31607b2d529431aface195d537d1d07253e4b198
Wintus <132e84e5@opayq.com>
2015-07-12 12:47:20 -0700

    Implement solve method

--- src/Chapter2/Section2/IntervalSchedulingProblem.java
+++ src/Chapter2/Section2/IntervalSchedulingProblem.java
@@ -1,13 +1,27 @@
 package Chapter2.Section2;
 
+import java.util.Arrays;
+
 /**
  * Using greedy algorithm.
  * Created by Yuya on 2015/07/12.
  */
-public class IntervalSchedulingProblem {
-    private int[][] schedule;
+class IntervalSchedulingProblem {
+    private final int[][] schedule; // {{start, end}}
 
     public IntervalSchedulingProblem(int[][] schedule) {
         this.schedule = schedule;
+    }
+
+    int solve() {
+        Arrays.sort(schedule, (a1, a2) -> Integer.compare(a1[1], a2[1]));
+        int n = 0, end = 0;
+        for (int[] aSchedule : schedule) {
+            if (end < aSchedule[0]) {
+                ++n;
+                end = aSchedule[1];
+            }
+        }
+        return n;
     }
 }
