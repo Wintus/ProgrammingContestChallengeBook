ee2b0ef0e7c3bdbe959cbaf6e261135d096f32e8
Wintus <132e84e5@opayq.com>
2015-07-12 13:29:16 -0700

    Create SarumanArmy class

---
+++ src/Chapter2/Section2/SarumanArmy.java
@@ -0,0 +1,17 @@
+package Chapter2.Section2;
+
+/**
+ * Greedy algorithm.
+ * Created by Yuya on 2015/07/12.
+ */
+public class SarumanArmy {
+    private int N;
+    private int R;
+    private int[] points;
+
+    public SarumanArmy(int n, int r, int[] points) {
+        N = n;
+        R = r;
+        this.points = points;
+    }
+}
