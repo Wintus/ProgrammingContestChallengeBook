80d470763d4cee0779f763151303e9d81766260c
Wintus <132e84e5@opayq.com>
2015-07-12 12:56:33 -0700

    Create BestCowLine class

---
+++ src/Chapter2/Section2/BestCowLine.java
@@ -0,0 +1,13 @@
+package Chapter2.Section2;
+
+/**
+ * Greedy Algorithm.
+ * Created by Yuya on 2015/07/12.
+ */
+public class BestCowLine {
+    char[] chars;
+
+    public BestCowLine(String string) {
+        chars = string.toCharArray();
+    }
+}
