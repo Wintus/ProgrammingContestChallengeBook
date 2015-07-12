773ab4222bc0d92e5ca55b49c9acd0bf2ee9d37c
Wintus <132e84e5@opayq.com>
2015-07-12 13:10:35 -0700

    Implement solve method

--- src/Chapter2/Section2/BestCowLine.java
+++ src/Chapter2/Section2/BestCowLine.java
@@ -1,5 +1,10 @@
 package Chapter2.Section2;
 
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Greedy Algorithm.
  * Created by Yuya on 2015/07/12.
@@ -9,5 +14,18 @@ public class BestCowLine {
 
     public BestCowLine(String string) {
         chars = string.toCharArray();
+    }
+
+    String solve() {
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : chars) deque.add(c);
+        StringWriter writer = new StringWriter();
+        while (deque.size() > 0) {
+            if (deque.peekFirst() < deque.peekLast())
+                writer.append(deque.pollFirst());
+            else
+                writer.append(deque.pollFirst());
+        }
+        return writer.toString();
     }
 }
