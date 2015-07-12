c76c44af15a8a8d0e890bc89bab9a8e15386587a
Wintus <132e84e5@opayq.com>
2015-07-11 17:10:48 -0700

    Create LakeCounting class

---
+++ src/Chapter2/LakeCounting.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+/**
+ * Using Deep First Search
+ * Created by Yuya on 2015/07/11.
+ */
+public class LakeCounting {
+    boolean[][] lake;
+
+    public LakeCounting(boolean[][] lake) {
+        this.lake = lake;
+    }
+}
