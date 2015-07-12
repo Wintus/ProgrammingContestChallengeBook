17da46ad40af405c7c1f4307945e0cb9ca187e57
Wintus <132e84e5@opayq.com>
2015-07-12 12:53:24 -0700

    Implement main method

--- src/Chapter2/Section2/IntervalSchedulingProblem.java
+++ src/Chapter2/Section2/IntervalSchedulingProblem.java
@@ -1,6 +1,8 @@
 package Chapter2.Section2;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 /**
  * Using greedy algorithm.
@@ -23,5 +25,17 @@ class IntervalSchedulingProblem {
             }
         }
         return n;
+    }
+
+    // @formatter:off
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int[][] schedule = new int[n][2];
+            IntStream.range(0, n).forEach(i ->
+                IntStream.range(0, 2).forEach(j ->
+                    schedule[i][j] = scanner.nextInt()));
+            System.out.println(new IntervalSchedulingProblem(schedule).solve());
+        }
     }
 }
