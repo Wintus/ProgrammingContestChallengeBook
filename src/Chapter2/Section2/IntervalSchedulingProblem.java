229b92bbe4f800f593ccb812bf3129bae9fcb5a2
Wintus <132e84e5@opayq.com>
2015-07-12 12:12:13 -0700

    Create IntervalSchedulingProblem class

---
+++ src/Chapter2/Section2/IntervalSchedulingProblem.java
@@ -0,0 +1,13 @@
+package Chapter2.Section2;
+
+/**
+ * Using greedy algorithm.
+ * Created by Yuya on 2015/07/12.
+ */
+public class IntervalSchedulingProblem {
+    private int[][] schedule;
+
+    public IntervalSchedulingProblem(int[][] schedule) {
+        this.schedule = schedule;
+    }
+}
