a8b587312ddce9bac7b5a30f2a3bce8733bd1f87
Wintus <132e84e5@opayq.com>
2015-07-13 23:21:41 -0700

    Implement main method

--- src/Chapter2/Chapter3/KnapsackProblem01.java
+++ src/Chapter2/Chapter3/KnapsackProblem01.java
@@ -1,16 +1,17 @@
 package Chapter2.Chapter3;
 
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 /**
  * Dynamic Programming.
  * Created by Yuya on 2015/07/13.
  */
-public class KnapsackProblem01 {
-    int[][] items; // {{weight, value}}
-    int weight;
-    int[][] dp; // {{nth item, weight}}
+class KnapsackProblem01 {
+    private final int[][] items; // {{weight, value}}
+    private final int weight;
+    private final int[][] dp; // {{nth item, weight}}
 
     public KnapsackProblem01(int[][] items, int weight) {
         this.items = items;
@@ -18,7 +19,7 @@ public class KnapsackProblem01 {
         dp = new int[items.length + 1][items[0].length + 1];
     }
 
-    int recursion(int n, int w) {
+    private int recursion(int n, int w) {
         if (dp[n][w] >= 0)
             return dp[n][w];
         int result;
@@ -36,5 +37,17 @@ public class KnapsackProblem01 {
         IntStream.range(0, dp.length)
                  .forEach(n -> Arrays.setAll(dp[n], x -> -1));
         return recursion(0, weight);
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int[][] items = new int[n][2];
+            // @formatter:off
+            IntStream.range(0, n).forEach(i ->
+                items[i] = new int[]{scanner.nextInt(), scanner.nextInt()});
+            int w = scanner.nextInt();
+            System.out.println(new KnapsackProblem01(items, w).solve());
+        }
     }
 }
