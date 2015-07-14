682812024ec766823a34698d8ba5435e46ade457
Wintus <132e84e5@opayq.com>
2015-07-14 00:40:04 -0700

    Implement solve method

--- src/Chapter2/Section3/LongestCommonSubsequence.java
+++ src/Chapter2/Section3/LongestCommonSubsequence.java
@@ -1,15 +1,33 @@
 package Chapter2.Section3;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Dynamic Programming.
  * Created by Yuya on 2015/07/14.
  */
-public class LongestCommonSubsequence {
-    String string0;
-    String string1;
+class LongestCommonSubsequence {
+    final String string0;
+    final String string1;
+    final int[][] dp;
 
     public LongestCommonSubsequence(String string0, String string1) {
         this.string0 = string0;
         this.string1 = string1;
+        int n = string0.length() + 1;
+        dp = new int[n][string1.length() + 1];
+        IntStream.range(0, n).forEach(i ->
+                Arrays.setAll(dp[i], x -> 0));
+    }
+
+    int solve() {
+        int n = string0.length(), m = string1.length();
+        // @formatter:off
+        IntStream.range(0, n).forEach(i ->
+            IntStream.range(0, m).forEach(j ->
+                dp[i + 1][j + 1] = string0.charAt(i) == string1.charAt(j) ?
+                    dp[i][j] + 1 : Math.max(dp[i][j + 1], dp[i + 1][j])));
+        return dp[n][m];
     }
 }
