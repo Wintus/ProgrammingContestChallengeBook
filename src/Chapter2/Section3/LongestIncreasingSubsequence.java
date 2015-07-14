5cef3f78d2fcebb4111ed6710be823b21c2e1bf9
Wintus <132e84e5@opayq.com>
2015-07-14 02:26:40 -0700

    Create LongestIncreasingSubsequence class

---
+++ src/Chapter2/Section3/LongestIncreasingSubsequence.java
@@ -0,0 +1,15 @@
+package Chapter2.Section3;
+
+/**
+ * Dynamic Programming.
+ * Created by Yuya on 2015/07/14.
+ */
+class LongestIncreasingSubsequence {
+    int[] numbers;
+    int[] dp;
+
+    public LongestIncreasingSubsequence(int[] numbers, int[] dp) {
+        this.numbers = numbers;
+        this.dp = dp;
+    }
+}
