51fba0260a35bfbe79f485a039df6a767e297154
Wintus <132e84e5@opayq.com>
2015-07-14 03:53:39 -0700

    Implement solve1 method that is much faster

--- src/Chapter2/Section3/LongestIncreasingSubsequence.java
+++ src/Chapter2/Section3/LongestIncreasingSubsequence.java
@@ -1,5 +1,8 @@
 package Chapter2.Section3;
 
+import Library.Lookup;
+
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -10,19 +13,22 @@ import java.util.stream.IntStream;
 class LongestIncreasingSubsequence {
     private final int[] numbers;
     private final int[] dp;
+    private int N;
 
     public LongestIncreasingSubsequence(int[] numbers) {
         this.numbers = numbers;
         dp = new int[numbers.length];
+        N = numbers.length;
     }
 
     /**
      * dp[i] = length of LIS that its last one is numbers[i].
+     * O(n^2).
      *
      * @return length of LIS.
      */
     int solve() {
-        return IntStream.range(0, numbers.length).map(i -> {
+        return IntStream.range(0, N).map(i -> {
             dp[i] = 1;
             IntStream.range(0, i).forEach(j -> {
                 if (numbers[j] < numbers[i])
@@ -32,11 +38,28 @@ class LongestIncreasingSubsequence {
         }).max().getAsInt();
     }
 
+    /**
+     * dp[i] = min of the last element of LIS in length i + 1 :? INF.
+     * O(n log n).
+     *
+     * @return length of LIS
+     */
+    int solve1() {
+        Integer[] dp = new Integer[N + 1];
+        final int INF = Integer.MAX_VALUE / 2;
+        Arrays.fill(dp, INF);
+        IntStream.range(0, N).forEach(i ->
+                Lookup.lower_bound(dp, numbers[i], x -> numbers[i]));
+        return Lookup.lower_bound(dp, INF);
+    }
+
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
             int[] nums = IntStream.range(0, n).map(i -> scanner.nextInt()).toArray();
-            System.out.println(new LongestIncreasingSubsequence(nums).solve());
+            LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence(nums);
+            System.out.println(lis.solve());
+            System.out.println(lis.solve1());
         }
     }
 }
