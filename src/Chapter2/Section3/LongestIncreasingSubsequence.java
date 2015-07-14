87b3036fb54990f4e41b0a8a8f741b70391608b7
Wintus <132e84e5@opayq.com>
2015-07-14 02:35:55 -0700

    Implement solve method

--- src/Chapter2/Section3/LongestIncreasingSubsequence.java
+++ src/Chapter2/Section3/LongestIncreasingSubsequence.java
@@ -1,5 +1,7 @@
 package Chapter2.Section3;
 
+import java.util.stream.IntStream;
+
 /**
  * Dynamic Programming.
  * Created by Yuya on 2015/07/14.
@@ -11,5 +13,21 @@ class LongestIncreasingSubsequence {
     public LongestIncreasingSubsequence(int[] numbers, int[] dp) {
         this.numbers = numbers;
         this.dp = dp;
+    }
+
+    /**
+     * dp[i] = length of LIS that its last one is numbers[i].
+     *
+     * @return length of LIS.
+     */
+    int solve() {
+        return IntStream.range(0, numbers.length).map(i -> {
+            dp[i] = 1;
+            IntStream.range(0, i).forEach(j -> {
+                if (numbers[j] < numbers[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            });
+            return dp[i];
+        }).max().getAsInt();
     }
 }
