ad723c7933b5239dcf846ee8fb6f853ef4e5e4af
Wintus <132e84e5@opayq.com>
2015-07-13 22:57:13 -0700

    Create 01KnapsackProblem class

---
+++ src/Chapter2/Chapter3/KnapsackProblem01.java
@@ -0,0 +1,15 @@
+package Chapter2.Chapter3;
+
+/**
+ * Dynamic Programming.
+ * Created by Yuya on 2015/07/13.
+ */
+public class KnapsackProblem01 {
+    int[][] items;
+    int weight;
+
+    public KnapsackProblem01(int[][] items, int weight) {
+        this.items = items;
+        this.weight = weight;
+    }
+}
