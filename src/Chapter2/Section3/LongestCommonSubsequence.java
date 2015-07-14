644563793930f0413e663e5f5351a7fb65330056
Wintus <132e84e5@opayq.com>
2015-07-14 00:30:34 -0700

    Create LongestCommonSubsequence class

---
+++ src/Chapter2/Section3/LongestCommonSubsequence.java
@@ -0,0 +1,15 @@
+package Chapter2.Section3;
+
+/**
+ * Dynamic Programming.
+ * Created by Yuya on 2015/07/14.
+ */
+public class LongestCommonSubsequence {
+    String string0;
+    String string1;
+
+    public LongestCommonSubsequence(String string0, String string1) {
+        this.string0 = string0;
+        this.string1 = string1;
+    }
+}
