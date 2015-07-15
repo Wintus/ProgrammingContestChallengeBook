662f0d310b68a50ff92339d5ca0b1a2a2f4bc470
Wintus <132e84e5@opayq.com>
2015-07-14 19:34:19 -0700

    Create Expedition class

---
+++ src/Chapter2/Section4/Expedition.java
@@ -0,0 +1,16 @@
+package Chapter2.Section4;
+
+/**
+ * Using Priority Queue.
+ * Created by Yuya on 2015/07/14.
+ */
+public class Expedition {
+    int L, P;
+    int[][] stands;
+
+    public Expedition(int l, int p, int[][] stands) {
+        L = l;
+        P = p;
+        this.stands = stands;
+    }
+}
