bed824bc47a2a0110239ad0236c884e37dfe976b
Wintus <132e84e5@opayq.com>
2015-07-14 19:12:56 -0700

    Implement solve method

--- src/Chapter2/Section3/RepeatedCombination.java
+++ src/Chapter2/Section3/RepeatedCombination.java
@@ -1,17 +1,32 @@
 package Chapter2.Section3;
 
+import java.util.stream.IntStream;
+
 /**
  * Dynamic Programming.
  * Created by Yuya on 2015/07/14.
  */
 public class RepeatedCombination {
-    int n;
+    int n, m;
     int[] a;
     int M;
+    int[][] dp;
 
     public RepeatedCombination(int n, int[] a, int m) {
         this.n = n;
         this.a = a;
+        this.m = a.length;
         M = m;
+        dp = new int[m + 1][n + 1];
+    }
+
+    int solve() {
+        IntStream.rangeClosed(0, m).forEach(i -> dp[i][0] = 1);
+        // @formatter:off
+        IntStream.range(0, m).forEach(i ->
+            IntStream.rangeClosed(1, n).forEach(j ->
+                dp[i + 1][j] = (dp[i + 1][j - 1] + dp[i][j] +
+                    (j - 1 - a[i] >= 0 ? M - dp[i][j - 1 - a[i]] : 0)) % M));
+        return dp[m][n];
     }
 }
