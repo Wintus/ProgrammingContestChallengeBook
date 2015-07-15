fe321746ca561c72b54fb0a662edb19ca01297c6
Wintus <132e84e5@opayq.com>
2015-07-15 11:14:24 -0700

    Create inner UnionFindTree class

--- src/Chapter2/Section4/FoodChain.java
+++ src/Chapter2/Section4/FoodChain.java
@@ -14,4 +14,38 @@ public class FoodChain {
         N = n;
         this.information = information;
     }
+
+    class UnionFindTree {
+        int[] parents, ranks;
+
+        public UnionFindTree(int n) {
+            parents = new int[n];
+            ranks = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 0;
+            }
+        }
+
+        int find(int i) {
+            if (parents[i] == i) return i;
+            else return parents[i] = find(parents[i]);
+        }
+
+        void unite(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x == y) return;
+            if (ranks[x] < ranks[y]) {
+                parents[x] = y;
+            } else {
+                parents[y] = x;
+                if (ranks[x] == ranks[y]) ranks[x]++;
+            }
+        }
+
+        boolean isSame(int x, int y) {
+            return find(x) == find(y);
+        }
+    }
 }
