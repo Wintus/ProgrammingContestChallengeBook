d4a55323a05fba38e98377c88dd4ff18e6f029b8
Wintus <132e84e5@opayq.com>
2015-07-14 20:13:20 -0700

    Create FenceRepair class

---
+++ src/Chapter2/Section4/FenceRepair.java
@@ -0,0 +1,13 @@
+package Chapter2.Section4;
+
+/**
+ * Using Huffman Coding.
+ * Created by Yuya on 2015/07/14.
+ */
+public class FenceRepair {
+    private int[] boards;
+
+    public FenceRepair(int[] boards) {
+        this.boards = boards;
+    }
+}
