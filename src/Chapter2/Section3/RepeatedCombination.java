9296c8f64b838a87ff1da3ba1089490fa1d43876
Wintus <132e84e5@opayq.com>
2015-07-14 18:59:17 -0700

    Create RepeatedCombination class

---
+++ src/Chapter2/Section3/RepeatedCombination.java
@@ -0,0 +1,17 @@
+package Chapter2.Section3;
+
+/**
+ * Dynamic Programming.
+ * Created by Yuya on 2015/07/14.
+ */
+public class RepeatedCombination {
+    int n;
+    int[] a;
+    int M;
+
+    public RepeatedCombination(int n, int[] a, int m) {
+        this.n = n;
+        this.a = a;
+        M = m;
+    }
+}
