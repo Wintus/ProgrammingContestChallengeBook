29a24a28aaf0d04f65deda7e9b2321692b13e97c
Wintus <132e84e5@opayq.com>
2015-07-14 18:38:07 -0700

    Implement main method

--- src/Chapter2/Section3/PartitionFunction.java
+++ src/Chapter2/Section3/PartitionFunction.java
@@ -1,5 +1,6 @@
 package Chapter2.Section3;
 
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 /**
@@ -14,7 +15,7 @@ public class PartitionFunction {
         this.n = n;
         this.m = m;
         M = m1;
-        dp = new int[n][m];
+        dp = new int[m + 1][n + 1];
     }
 
     /**
@@ -23,11 +24,21 @@ public class PartitionFunction {
      * @return mod m of m partition of n.
      */
     int solve() {
+        dp[0][0] = 1;
         IntStream.rangeClosed(1, m).forEach(i ->
                 IntStream.rangeClosed(0, n).forEach(j ->
                         dp[i][j] = j - i >= 0 ?
-                                dp[i - 1][j] + dp[i][j - i] % M :
+                                (dp[i - 1][j] + dp[i][j - i]) % M :
                                 dp[i - 1][j]));
         return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int M = scanner.nextInt();
+            System.out.println(new PartitionFunction(n, m, M).solve());
+        }
     }
 }
