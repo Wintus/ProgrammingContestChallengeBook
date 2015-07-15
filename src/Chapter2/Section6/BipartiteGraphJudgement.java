2631200d201688f14cb936f58247c0b3670b8b88
Wintus <132e84e5@opayq.com>
2015-07-15 14:17:32 -0700

    Create BipartiteGraphJudgement class

---
+++ src/Chapter2/Section6/BipartiteGraphJudgement.java
@@ -0,0 +1,13 @@
+package Chapter2.Section6;
+
+/**
+ * Graph.
+ * Created by Yuya on 2015/07/15.
+ */
+public class BipartiteGraphJudgement {
+    int[][] graph;
+
+    public BipartiteGraphJudgement(int[][] graph) {
+        this.graph = graph;
+    }
+}
