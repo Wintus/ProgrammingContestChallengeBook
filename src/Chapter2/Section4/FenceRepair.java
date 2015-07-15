5483f9f88d21548ad062680537d5fdba9ca739e4
Wintus <132e84e5@opayq.com>
2015-07-14 20:18:26 -0700

    Implement solve method

--- src/Chapter2/Section4/FenceRepair.java
+++ src/Chapter2/Section4/FenceRepair.java
@@ -1,5 +1,7 @@
 package Chapter2.Section4;
 
+import java.util.PriorityQueue;
+
 /**
  * Using Huffman Coding.
  * Created by Yuya on 2015/07/14.
@@ -9,5 +11,20 @@ public class FenceRepair {
 
     public FenceRepair(int[] boards) {
         this.boards = boards;
+    }
+
+    int solve() {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int board : boards) queue.add(board);
+        while (queue.size() > 1) {
+            int b0, b1;
+            b0 = queue.poll();
+            b1 = queue.poll();
+            int b = b0 + b1;
+            answer += b;
+            queue.add(b);
+        }
+        return answer;
     }
 }
