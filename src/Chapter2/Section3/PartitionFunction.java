f7b073e73cb5d48a4cfc3a4861a65929f205a88b
Wintus <132e84e5@opayq.com>
2015-07-14 18:31:33 -0700

    Implement solve method

--- src/Chapter2/Section3/PartitionFunction.java
+++ src/Chapter2/Section3/PartitionFunction.java
@@ -1,5 +1,7 @@
 package Chapter2.Section3;
 
+import java.util.stream.IntStream;
+
 /**
  * Dynamic Programming.Partial Function of Mod M.
  * Created by Yuya on 2015/07/14.
@@ -13,5 +15,19 @@ public class PartitionFunction {
         this.m = m;
         M = m1;
         dp = new int[n][m];
+    }
+
+    /**
+     * dp[i][j] = total number of i partition of j.
+     *
+     * @return mod m of m partition of n.
+     */
+    int solve() {
+        IntStream.rangeClosed(1, m).forEach(i ->
+                IntStream.rangeClosed(0, n).forEach(j ->
+                        dp[i][j] = j - i >= 0 ?
+                                dp[i - 1][j] + dp[i][j - i] % M :
+                                dp[i - 1][j]));
+        return dp[m][n];
     }
 }
