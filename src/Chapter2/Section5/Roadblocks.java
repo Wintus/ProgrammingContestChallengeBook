890725b4509554ead39ed91d9e0b88cdfd5c1056
Wintus <132e84e5@opayq.com>
2015-07-23 00:08:45 -0700

    Implement solve method

--- src/Chapter2/Section5/Roadblocks.java
+++ src/Chapter2/Section5/Roadblocks.java
@@ -1,6 +1,8 @@
 package Chapter2.Section5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * Graph algorithm.
@@ -15,19 +17,48 @@ public class Roadblocks {
             this.to = to;
             this.cost = cost;
         }
-
-        @Override
-        public String toString() {
-            return from + " -> " + to + ": " + cost;
-        }
     }
 
-    private int N, R;
+    private int N;
     private ArrayList<Edge> edges;
+    private static final int INF = Integer.MAX_VALUE / 2;
 
-    public Roadblocks(int n, int r, ArrayList<Edge> edges) {
+    public Roadblocks(int n, ArrayList<Edge> edges) {
         N = n;
-        R = r;
         this.edges = edges;
+    }
+
+    int solve() {
+        int[] distance = new int[N];
+        int[] distance2 = new int[N];
+        PriorityQueue<Edge> queue = new PriorityQueue<>((e0, e1) ->
+                Integer.compare(e0.cost, e1.cost));
+        Arrays.fill(distance, INF);
+        Arrays.fill(distance2, INF);
+        distance[0] = 0;
+        queue.add(new Edge(-1, 0, 0));
+
+        while (!queue.isEmpty()) {
+            Edge poll = queue.poll();
+            int v = poll.to;
+            if (distance2[v] < poll.cost) continue;
+            edges.stream().filter(edge -> edge.from == v)
+                 .forEach(edge -> {
+                     int d2 = poll.cost + edge.cost;
+                     if (distance[edge.to] > d2) {
+                         // swap
+                         int dummy = distance[edge.to];
+                         distance[edge.to] = d2;
+                         d2 = dummy;
+                         queue.add(new Edge(-1, edge.to, distance[edge.to]));
+                     }
+                     if (distance2[edge.to] > d2 && distance[edge.to] < d2) {
+                         distance2[edge.to] = d2;
+                         queue.add(new Edge(-1, edge.to, distance2[edge.to]));
+                     }
+                 });
+        }
+
+        return distance2[distance2.length - 1];
     }
 }
