39fb547d50cd763abc117b0890c6329d079a3278
Wintus <132e84e5@opayq.com>
2015-07-26 04:46:50 -0700

    Refactor a bit

--- src/Library/Timer.java
+++ src/Library/Timer.java
@@ -1,8 +1,12 @@
 package Library;
 
 import java.text.NumberFormat;
-import java.util.*;
+import java.util.Locale;
+import java.util.SortedMap;
+import java.util.StringJoiner;
+import java.util.TreeMap;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Performance measure.
@@ -26,12 +30,12 @@ public class Timer<T> {
 
     public static void main(String[] args) {
         Timer<Object> timer = new Timer<>();
-        String format;
+        Function<Long, String> formatter = n ->
+                NumberFormat.getNumberInstance(Locale.US).format(n);
 
         // measure in ƒÊs
         long time0 = timer.run(0, i -> System.out.println("Timer")) / 1000;
-        format = NumberFormat.getNumberInstance(Locale.US).format(time0);
-        System.out.println(format + "\u00B5s");
+        System.out.println(time0 + "\u00B5s");
         System.out.println();
 
         long start = System.nanoTime();
@@ -39,8 +43,7 @@ public class Timer<T> {
         long end = System.nanoTime();
         long time1 = end - start;
         time1 /= 1000;
-        format = NumberFormat.getNumberInstance(Locale.US).format(time1);
-        System.out.println(format + "\u00B5s");
+        System.out.println(time1 + "\u00B5s");
         System.out.println();
 
         long d = Math.abs(time0 - time1);
@@ -56,10 +59,17 @@ public class Timer<T> {
             end = System.nanoTime();
             d = end - start;
             times.put(d, times.getOrDefault(d, 0) + 1);
-            d = timer.run(0, o -> {});
+            // @formatter:off
+            d = timer.run(0, o -> {}); // minimal consumer of no-op
             runs.put(d, times.getOrDefault(d, 0) + 1);
         }
-        System.out.println("times:\t" + times); // < .5-5ms
-        System.out.println("runs:\t" + runs);   // < 5-30ms
+        StringJoiner t = new StringJoiner("; ", "[", "]");
+        StringJoiner r = new StringJoiner("; ", "[", "]");
+        times.forEach((time, count) ->
+                t.add(formatter.apply(time) + "=" + count));
+        runs.forEach((time, count) ->
+                r.add(formatter.apply(time) + "=" + count));
+        System.out.println("times:\t" + t); // < .5-5ms
+        System.out.println("runs:\t" + r); // < 5-30ms
     }
 }
