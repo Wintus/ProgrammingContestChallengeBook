06add800111249b202a5a8982f73708c7f4a73d0
Wintus <132e84e5@opayq.com>
2015-07-26 02:31:46 -0700

    Create Timer class

---
+++ src/Library/Timer.java
@@ -0,0 +1,33 @@
+package Library;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+/**
+ * Performance measure.
+ * Created by Yuya on 2015/07/26.
+ */
+public class Timer<T> {
+    public long run(T t, Consumer<T> consumer) {
+        long start = System.nanoTime();
+        consumer.accept(t);
+        long end = System.nanoTime();
+        return end - start;
+    }
+
+    public static void main(String[] args) {
+        Timer<Integer> timer = new Timer<>();
+        long time = timer.run(0, i -> System.out.println("Timer"));
+        String format = NumberFormat.getNumberInstance(Locale.US).format(time);
+        System.out.println(format + " ns");
+        System.out.println();
+
+        long start = System.nanoTime();
+        System.out.println("Timer");
+        long end = System.nanoTime();
+        time = end - start;
+        format = NumberFormat.getNumberInstance(Locale.US).format(time);
+        System.out.println(format + " ns");
+    }
+}
