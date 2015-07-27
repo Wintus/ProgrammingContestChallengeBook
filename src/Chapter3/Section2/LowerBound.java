4d5e6818a7e3f1a75105fb625edf26ef5105ee9a
Wintus <132e84e5@opayq.com>
2015-07-26 20:16:28 -0700

    Create LowerBound class

---
+++ src/Chapter3/Section2/LowerBound.java
@@ -0,0 +1,55 @@
+package Chapter3.Section2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Binary Search.
+ * Find the smallest index that a[i] >= k from a increasing number line.
+ * Created by Yuya on 2015/07/26.
+ */
+public class LowerBound {
+    private final int[] ns;
+    private final int k;
+
+    public LowerBound(int[] ns, int k) {
+        this.ns = ns;
+        this.k = k;
+    }
+
+    int solve() {
+        int lowerBound = -1, upperBound = ns.length - 1, middle;
+        while (upperBound - lowerBound > 1) {
+            middle = (lowerBound + upperBound) / 2;
+            if (ns[middle] >= k)
+                upperBound = middle; // solution in (lb, mid]
+            else
+                lowerBound = middle; // solution in (mid, ub]
+        }
+        return upperBound;
+    }
+
+    int solve1() {
+        int lowerBound = 0, upperBound = ns.length - 1, middle = 0;
+        while (lowerBound <= upperBound) {
+            middle = (upperBound + lowerBound) / 2;
+            if (ns[middle] >= k)
+                upperBound = middle - 1; // solution is in [lb, mid)
+            else
+                lowerBound = middle + 1; // solution is in (mid, ub]
+        }
+        return middle;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int[] ns = new int[n];
+            Arrays.setAll(ns, x -> scanner.nextInt());
+            int k = scanner.nextInt();
+            LowerBound lowerBound = new LowerBound(ns, k);
+            System.out.println(lowerBound.solve());
+            System.out.println(lowerBound.solve1());
+        }
+    }
+}
