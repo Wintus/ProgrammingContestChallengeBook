eef2f03aea6bbccd7266f346e7e35ceb4942a871
Wintus <132e84e5@opayq.com>
2015-08-05 00:07:03 -0700

    Implement solve method of recursive version

--- src/Chapter3/Section5/TravelingSalesmanProblem.java
+++ src/Chapter3/Section5/TravelingSalesmanProblem.java
@@ -1,5 +1,7 @@
 package Chapter3.Section5;
 
+import java.util.Arrays;
+
 /**
  * Master Dynamic Programming of Bit DP.
  * Created by Yuya on 2015/08/04.
@@ -14,5 +16,28 @@ public class TravelingSalesmanProblem {
         N = n;
         this.distance = distance;
         dp = new int[1 << N][N];
+    }
+
+    // set is the set of visited vertexes. v is the current vertex.
+    private int recursion(int set, int v) {
+        if (dp[set][v] >= 0)
+            return dp[set][v];
+        if (set == (1 << N) - 1 && v == 0)
+            // traversed all vertexes
+            return dp[set][v] = 0;
+
+        int result = Integer.MAX_VALUE;
+        for (int u = 0; u < N; u++) {
+            if (!((set >> u & 1) == 1))
+                // go next
+                result = Math.min(result,
+                        recursion(set | 1 << u, u) + distance[v][u]);
+        }
+        return dp[set][v] = result;
+    }
+
+    int solveRec() {
+        for (int[] ints : dp) Arrays.fill(ints, -1);
+        return recursion(0, 0);
     }
 }
