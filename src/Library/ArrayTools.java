66fe187f2c7db36abbabe2246afcc3003da3c1bd
Wintus <132e84e5@opayq.com>
2015-08-08 15:40:14 -0700

    Create ArrayTools class

---
+++ src/Library/ArrayTools.java
@@ -0,0 +1,21 @@
+package Library;
+
+import java.util.Arrays;
+
+/**
+ * Array Tools.
+ * Created by Yuya on 2015/08/08.
+ */
+public class ArrayTools {
+    public static void fill2d(int[][] array, int value) {
+        for (int[] ints : array) Arrays.fill(ints, value);
+    }
+
+    public static void fill2d(double[][] array, double value) {
+        for (double[] doubles : array) Arrays.fill(doubles, value);
+    }
+
+    public static void fill2d(long[][] array, long value) {
+        for (long[] longs : array) Arrays.fill(longs, value);
+    }
+}
