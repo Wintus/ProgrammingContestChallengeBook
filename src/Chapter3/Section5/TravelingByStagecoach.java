a15b2171e4ea093777cf2cf2a84c4df68fb350b1
Wintus <132e84e5@opayq.com>
2015-08-08 15:45:51 -0700

    Bug fix and Refactor naming

--- src/Chapter3/Section5/TravelingByStagecoach.java
+++ src/Chapter3/Section5/TravelingByStagecoach.java
@@ -1,35 +1,35 @@
 package Chapter3.Section5;
 
-import java.util.Arrays;
+import Library.ArrayTools;
 
 /**
  * Master Dynamic Programming. DAG.
  * Created by Yuya on 2015/08/06.
  */
 public class TravelingByStagecoach {
-    private final int N, M, A, B;
+    private final int N, M, start, goal;
     private final int[] horse;
     private final int[][] distance;
-    // Bit DP of minimum costs to be at v with remaining tickets S
+    // Bit DP of minimum costs to be at v with remaining ticket set
     private final double[][] dp;
     private static final double INF = Double.MAX_VALUE / 2;
 
-    public TravelingByStagecoach(int n, int m, int a, int b, int[] horse) {
-        N = n; // number of tickets
-        M = m; // number of roads
-        A = a; // start
-        B = b; // goal
+    public TravelingByStagecoach(int m, int start, int goal, int[] horse, int[][] dis) {
+        N = horse.length; // number of tickets
+        M = m; // number of cities
+        this.start = start - 1; // start, indexed
+        this.goal = goal - 1; // goal, indexed
         this.horse = horse; // numbers of horses
-        distance = new int[M][M]; // -1 if no path
+        distance = dis; // -1 if no path
         dp = new double[1 << N][M];
     }
 
     double solve() {
-        for (double[] doubles : dp) Arrays.fill(doubles, INF);
-        dp[dp.length - 1][A - 1] = 0;
+        ArrayTools.fill2d(dp, INF);
+        dp[dp.length - 1][start] = 0;
         double result = INF;
         for (int set = (1 << N) - 1; set >= 0; set--) {
-            result = Math.min(result, (int) dp[set][B - 1]);
+            result = Math.min(result, dp[set][goal]);
             for (int v = 0; v < M; v++)
                 for (int i = 0; i < N; i++)
                     if ((set >> i & 1) == 1)
@@ -38,17 +38,16 @@ public class TravelingByStagecoach {
                             if (distance[v][u] >= 0)
                                 dp[set & ~(1 << i)][u] = Math.min(
                                         dp[set & ~(1 << i)][u],
-                                        dp[set][v] + (double)
-                                                distance[v][u] / horse[i]);
+                                        dp[set][v] + (double) distance[v][u] / horse[i]);
         }
         return result;
     }
 
     public static void main(String[] args) {
         int n = 2, m = 4, a = 2, b = 1;
-        int[] t = {3, 1};
+        int[] t = new int[]{3, 1};
         int[][] d = new int[m][m];
-        for (int[] ints : d) Arrays.fill(ints, -1);
+        ArrayTools.fill2d(d, -1);
         d[0][2] = 3;
         d[0][3] = 2;
         d[1][2] = 3;
@@ -58,7 +57,7 @@ public class TravelingByStagecoach {
         d[3][0] = 2;
         d[3][1] = 5;
 
-        double result = new TravelingByStagecoach(n, m, a, b, t).solve();
+        double result = new TravelingByStagecoach(m, a, b, t, d).solve();
 
         if (result == TravelingByStagecoach.INF) // unreachable
             System.out.println("Impossible");
